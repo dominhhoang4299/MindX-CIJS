e9360d6f7c824c5d7d2ab15ec864072848d1ad01
Hoang <dominhhoang4299@gmail.com>
2021-04-19 22:08:20 +0700

    commit message

---
+++ Lec-I/script.js
@@ -0,0 +1,20 @@
+const navItems = [{
+        name: "Item 1",
+    },
+    {
+        name: "Item 2",
+    },
+    {
+        name: "Item 3",
+    },
+    {
+        name: "Item 4",
+    }
+]
+
+window.onload = function() {
+    for (let a of navItems) {
+        document.getElementsByClassName("menu")[0].innerHTML += `<span>` + a.name + `</span>`;
+
+    }
+}
